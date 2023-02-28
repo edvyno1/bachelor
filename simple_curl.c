3df494d366d1cfe6d6e8af0bf73456e6e21006a6
Edvinas Gaubas <e.gaubas18@gmail.com>
2023-02-28 18:40:00 +0200

    add more stuff from example

--- simple_curl.c
+++ simple_curl.c
@@ -5,6 +5,17 @@
 #include <stdlib.h>
 #include <string.h>
  
+
+/* expected hook */
+PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
+	return PAM_SUCCESS;
+}
+
+PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+	printf("Acct mgmt\n");
+	return PAM_SUCCESS;
+}
+
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags,int argc, const char **argv)
 {
 
