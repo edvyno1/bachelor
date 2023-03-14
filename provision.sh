c68ff97340be9e9b39363f6dcebfb68921ceea6c
Edvinas Gaubas <e.gaubas18@gmail.com>
2023-03-14 20:06:11 +0200

    add full provision with dos2unix conversion

--- provision.sh
+++ provision.sh
@@ -3,7 +3,13 @@ set -euxo pipefail
 
 # Deps
 sudo apt-get update --allow-releaseinfo-change
-sudo apt-get -y install libcurl4-gnutls-dev libpam0g-dev
+sudo apt-get -y install libcurl4-gnutls-dev libpam0g-dev dos2unix
 
 # Python
 sudo apt-get -y install python3-pip python3-flask
+
+# Set up secondary user; salSp1wOPp6fk = encrypted 'test'
+sudo useradd -m -p salSp1wOPp6fk test
+sudo usermod -aG sudo test
+
+find /vagrant -not -path "/vagrant/.vagrant/*" -type f -print0 | xargs -0 dos2unix -ic0 | xargs -0 dos2unix -b
